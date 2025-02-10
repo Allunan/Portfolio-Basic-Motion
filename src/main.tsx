04f18a0e4ce68472799a4f6c34428dc6f4c3d002
Orewa <74195596+Orewajs@users.noreply.github.com>
2025-02-10 18:07:07 +0100

    Initialize develop

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import "@/styles/globals.css"
+
+import { Routes } from "@generouted/react-router"
+import { ThemeProvider } from "next-themes"
+import { createRoot } from "react-dom/client"
+import { StrictMode } from "react"
+
+const container = document.getElementById("root")!
+createRoot(container).render(
+  <StrictMode>
+    <ThemeProvider>
+      <Routes />
+    </ThemeProvider>
+  </StrictMode>
+)
