cde9aaa54d863036dbf050e701e602bc482425cb
elgharbi <elgharbiay@gmail.com>
2025-02-15 01:37:53 +0100

    feat: add About section component and integrate it into the main page

---
+++ src/components/sections/about.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import { cn } from "@/utils"
+import { Image } from "../shared/image"
+
+interface Props {
+  data: {
+    bio: string
+    hero_picture: string
+  }
+}
+
+export const About: React.FC<Props> = ({ data }) => {
+  return (
+    <section className="pt-60 pb-32 mx-[var(--grid-margins)] border-b border-dashed border-[#AFAFB6] border-opacity-70">
+      <div className="flex flex-wrap gap-base-2">
+        <div>
+          <h3 className="span-w-2 sm:span-w-1 lg:span-w-1 text-[#AFAFB6]">
+            about me
+          </h3>
+        </div>
+        <div>
+          <p className="span-w-2 aspect-[5] sm:span-w-3 lg:span-w-2 text-justify">
+            {data.bio}
+          </p>
+        </div>
+
+        <div className="span-w-2 sm:span-w-4 lg:span-w-5 flex justify-center mt-8 sm:mt-4 lg:mt-0">
+          <Image
+            alt=""
+            src={data.hero_picture}
+            className="span-w-2 sm:span-w-4 lg:span-w-3 aspect-[3]"
+          />
+          {/* <div
+            className={cn(" bg-cover bg-top", "bg-cover bg-no-repeat bg-top")}
+            style={{ backgroundImage: `url(hero.png)` }}></div> */}
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,13 @@ import { Footer } from "@/components/sections"
 import { Header } from "@/components/sections/header"
 import { Skills } from "@/components/sections/skills"
 import { Works } from "@/components/sections/works"
+import { About } from "@/components/sections/about"
 
 const Index: React.FC = () => {
   return (
     <>
       <Header data={data} />
+      <About data={data} />
       <Works data={data.works} />
       <Skills data={data.skills} />
       <Footer data={data} />
